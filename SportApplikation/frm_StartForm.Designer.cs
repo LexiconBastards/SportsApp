f9af0b61baf92d6bf18f347c5caa9a77ee01a08f
David Nilsson <david822nilsson@gmail.com>
2025-08-14 15:51:50 +0200

    Add initial project files

---
+++ SportApplikation/frm_StartForm.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace SportApplikation
+{
+    partial class frm_StartForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_StartForm));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.txtOneShoosedTeams = new System.Windows.Forms.TextBox();
+            this.txtTwoShoosedTeams = new System.Windows.Forms.TextBox();
+            this.txtThreeShoosedTeams = new System.Windows.Forms.TextBox();
+            this.txtFourShoosedTeams = new System.Windows.Forms.TextBox();
+            this.dataGridView_ShooseTeam = new System.Windows.Forms.DataGridView();
+            this.dataGridView_GameSchemeAndResults = new System.Windows.Forms.DataGridView();
+            this.dataGridView_Table = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox_Hockey = new System.Windows.Forms.PictureBox();
+            this.pictureBox_BasketBall = new System.Windows.Forms.PictureBox();
+            this.pictureBox_SoccerWomen = new System.Windows.Forms.PictureBox();
+            this.btn_CreateSchemeResultTable = new System.Windows.Forms.Button();
+            this.btn_AddNewTeam = new System.Windows.Forms.Button();
+            this.btn_Quit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_ShooseTeam)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_GameSchemeAndResults)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Table)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Hockey)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_BasketBall)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_SoccerWomen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(72, 74);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(290, 28);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // txtOneShoosedTeams
+            // 
+            this.txtOneShoosedTeams.Location = new System.Drawing.Point(72, 140);
+            this.txtOneShoosedTeams.Name = "txtOneShoosedTeams";
+            this.txtOneShoosedTeams.Size = new System.Drawing.Size(289, 26);
+            this.txtOneShoosedTeams.TabIndex = 1;
+            // 
+            // txtTwoShoosedTeams
+            // 
+            this.txtTwoShoosedTeams.Location = new System.Drawing.Point(72, 189);
+            this.txtTwoShoosedTeams.Name = "txtTwoShoosedTeams";
+            this.txtTwoShoosedTeams.Size = new System.Drawing.Size(289, 26);
+            this.txtTwoShoosedTeams.TabIndex = 2;
+            // 
+            // txtThreeShoosedTeams
+            // 
+            this.txtThreeShoosedTeams.Location = new System.Drawing.Point(72, 243);
+            this.txtThreeShoosedTeams.Name = "txtThreeShoosedTeams";
+            this.txtThreeShoosedTeams.Size = new System.Drawing.Size(289, 26);
+            this.txtThreeShoosedTeams.TabIndex = 3;
+            // 
+            // txtFourShoosedTeams
+            // 
+            this.txtFourShoosedTeams.Location = new System.Drawing.Point(72, 294);
+            this.txtFourShoosedTeams.Name = "txtFourShoosedTeams";
+            this.txtFourShoosedTeams.Size = new System.Drawing.Size(289, 26);
+            this.txtFourShoosedTeams.TabIndex = 4;
+            // 
+            // dataGridView_ShooseTeam
+            // 
+            this.dataGridView_ShooseTeam.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_ShooseTeam.Location = new System.Drawing.Point(391, 74);
+            this.dataGridView_ShooseTeam.Name = "dataGridView_ShooseTeam";
+            this.dataGridView_ShooseTeam.Size = new System.Drawing.Size(171, 512);
+            this.dataGridView_ShooseTeam.TabIndex = 5;
+            // 
+            // dataGridView_GameSchemeAndResults
+            // 
+            this.dataGridView_GameSchemeAndResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_GameSchemeAndResults.Location = new System.Drawing.Point(592, 74);
+            this.dataGridView_GameSchemeAndResults.Name = "dataGridView_GameSchemeAndResults";
+            this.dataGridView_GameSchemeAndResults.Size = new System.Drawing.Size(377, 283);
+            this.dataGridView_GameSchemeAndResults.TabIndex = 6;
+            // 
+            // dataGridView_Table
+            // 
+            this.dataGridView_Table.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_Table.Location = new System.Drawing.Point(592, 389);
+            this.dataGridView_Table.Name = "dataGridView_Table";
+            this.dataGridView_Table.Size = new System.Drawing.Size(377, 197);
+            this.dataGridView_Table.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(76, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Välj sport";
+            //this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(76, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Valda lag";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(397, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(162, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Klicka på önskade lag";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(588, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(184, 20);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Spelschema och resultat";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(588, 366);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Tabell";
+            // 
+            // pictureBox_Hockey
+            // 
+            this.pictureBox_Hockey.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox_Hockey.InitialImage")));
+            this.pictureBox_Hockey.Location = new System.Drawing.Point(1005, 80);
+            this.pictureBox_Hockey.Name = "pictureBox_Hockey";
+            this.pictureBox_Hockey.Size = new System.Drawing.Size(159, 156);
+            this.pictureBox_Hockey.TabIndex = 13;
+            this.pictureBox_Hockey.TabStop = false;
+            // 
+            // pictureBox_BasketBall
+            // 
+            this.pictureBox_BasketBall.InitialImage = null;
+            this.pictureBox_BasketBall.Location = new System.Drawing.Point(1005, 256);
+            this.pictureBox_BasketBall.Name = "pictureBox_BasketBall";
+            this.pictureBox_BasketBall.Size = new System.Drawing.Size(159, 156);
+            this.pictureBox_BasketBall.TabIndex = 14;
+            this.pictureBox_BasketBall.TabStop = false;
+            // 
+            // pictureBox_SoccerWomen
+            // 
+            this.pictureBox_SoccerWomen.InitialImage = null;
+            this.pictureBox_SoccerWomen.Location = new System.Drawing.Point(1005, 436);
+            this.pictureBox_SoccerWomen.Name = "pictureBox_SoccerWomen";
+            this.pictureBox_SoccerWomen.Size = new System.Drawing.Size(159, 150);
+            this.pictureBox_SoccerWomen.TabIndex = 15;
+            this.pictureBox_SoccerWomen.TabStop = false;
+            // 
+            // btn_CreateSchemeResultTable
+            // 
+            this.btn_CreateSchemeResultTable.Location = new System.Drawing.Point(80, 349);
+            this.btn_CreateSchemeResultTable.Name = "btn_CreateSchemeResultTable";
+            this.btn_CreateSchemeResultTable.Size = new System.Drawing.Size(283, 63);
+            this.btn_CreateSchemeResultTable.TabIndex = 16;
+            this.btn_CreateSchemeResultTable.Text = "Skapa spelschema med resultat och tabell";
+            this.btn_CreateSchemeResultTable.UseVisualStyleBackColor = true;
+            // 
+            // btn_AddNewTeam
+            // 
+            this.btn_AddNewTeam.Location = new System.Drawing.Point(78, 436);
+            this.btn_AddNewTeam.Name = "btn_AddNewTeam";
+            this.btn_AddNewTeam.Size = new System.Drawing.Size(283, 63);
+            this.btn_AddNewTeam.TabIndex = 17;
+            this.btn_AddNewTeam.Text = "Lägg till nya lag";
+            this.btn_AddNewTeam.UseVisualStyleBackColor = true;
+            this.btn_AddNewTeam.Click += new System.EventHandler(this.btn_AddNewTeam_Click);
+            // 
+            // btn_Quit
+            // 
+            this.btn_Quit.Location = new System.Drawing.Point(78, 523);
+            this.btn_Quit.Name = "btn_Quit";
+            this.btn_Quit.Size = new System.Drawing.Size(283, 63);
+            this.btn_Quit.TabIndex = 18;
+            this.btn_Quit.Text = "Avsluta";
+            this.btn_Quit.UseVisualStyleBackColor = true;
+            this.btn_Quit.Click += new System.EventHandler(this.btn_Quit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1734, 957);
+            this.Controls.Add(this.btn_Quit);
+            this.Controls.Add(this.btn_AddNewTeam);
+            this.Controls.Add(this.btn_CreateSchemeResultTable);
+            this.Controls.Add(this.pictureBox_SoccerWomen);
+            this.Controls.Add(this.pictureBox_BasketBall);
+            this.Controls.Add(this.pictureBox_Hockey);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView_Table);
+            this.Controls.Add(this.dataGridView_GameSchemeAndResults);
+            this.Controls.Add(this.dataGridView_ShooseTeam);
+            this.Controls.Add(this.txtFourShoosedTeams);
+            this.Controls.Add(this.txtThreeShoosedTeams);
+            this.Controls.Add(this.txtTwoShoosedTeams);
+            this.Controls.Add(this.txtOneShoosedTeams);
+            this.Controls.Add(this.comboBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Lagsport";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_ShooseTeam)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_GameSchemeAndResults)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Table)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Hockey)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_BasketBall)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_SoccerWomen)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox txtOneShoosedTeams;
+        private System.Windows.Forms.TextBox txtTwoShoosedTeams;
+        private System.Windows.Forms.TextBox txtThreeShoosedTeams;
+        private System.Windows.Forms.TextBox txtFourShoosedTeams;
+        private System.Windows.Forms.DataGridView dataGridView_ShooseTeam;
+        private System.Windows.Forms.DataGridView dataGridView_GameSchemeAndResults;
+        private System.Windows.Forms.DataGridView dataGridView_Table;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox_Hockey;
+        private System.Windows.Forms.PictureBox pictureBox_BasketBall;
+        private System.Windows.Forms.PictureBox pictureBox_SoccerWomen;
+        private System.Windows.Forms.Button btn_CreateSchemeResultTable;
+        private System.Windows.Forms.Button btn_AddNewTeam;
+        private System.Windows.Forms.Button btn_Quit;
+    }
+}
+
---
+++ SportApplikation/frm_StartForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SportApplikation
+{
+    public partial class frm_StartForm : Form
+    {
+        public frm_StartForm()
+        {
+            InitializeComponent();
+        }
+
+        //private void Label1_Click(object sender, EventArgs e)
+        //{
+
+        //}
+
+        private void btn_Quit_Click(object sender, EventArgs e)
+        {
+            // Closing the application
+            Application.Exit();
+        }
+
+        private void btn_AddNewTeam_Click(object sender, EventArgs e)
+        {
+            // Let the user add a new team to the database in the SQL-server by the form "frm_Team"
+            //AddNewTeam();
+
+        }
+    }
+}
